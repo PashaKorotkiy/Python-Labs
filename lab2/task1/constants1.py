b5b9e7bbe96c7c4e3eb816edc87feb45ce64d2b4
golden <planetrocky@list.ru>
2023-05-08 18:00:01 +0300

    added constants1.py

---
+++ lab2/task1/constants1.py
@@ -0,0 +1,21 @@
+# Path to files
+
+TEXT_PARSE_PATH = "data/text_parse.txt"
+TESTS = "tests/"
+
+# Shortcuts
+
+YES = "y"
+NO = "n"
+
+# Abbreviations
+
+APPEAL_ABBREVIATIONS = (r"\bMr\.", r"\bMrs\.", r"Dr\.", r"St\.", r"Blvd\.", r"Ave\.", r"Sq\.", r"Rd\.", r"Bldg\.",
+                        r"B\.Sc\.", r"M\.A\.", r"Ph\.D\.", r"M\.D\.", r"LT\.", r"Mx\.", r"Esq\.", r"Dr\.", r"KC\.",
+                        r"Fr\.", r"Pr\.", r"Br\.", r"Sr\.", r"Ph\.D\.")
+
+ABBREVIATIONS = (r"etc\.", r"Re\.", r"p\.", r"exp\.", r"err\.", r"et\.al\.", r"ex\.", r"e\.g\.", r"fin\.", r"i\.e\.",
+                 r"vs\.", r"N\.B\.", r"P\.S\.", r"P\.P\.S\.", r"P\.S\.S\.", r"ft\.", r"oz\.", r"pt\.", r"in\.",
+                 r"sec\.", r"\bg\.", r"cm\.", r"qt\.", r"p\.m\.")
+
+# Regular expr
